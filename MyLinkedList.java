d05fa4c9498dc3e0e1e3c27ef005378248b51669
John Dogit config --global user.name John <jlee01@stuy.edu>
2018-12-13 14:52:19 -0500

    end of period

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -46,17 +46,46 @@ public class MyLinkedList{
   }
   public boolean add(int value){
     Node oldEnd = end;
+    end = new Node(value, null , oldEnd);
+    end.prev().setNext(end);
     size++;
-    end = new Node(value, end, oldEnd);
     return true;
   }
   public String toString(){
     String output = "";
     Node x = this.start;
-    for (int s = 0; s < size ; s++ ){
-      output += x.data();
-      x = x.next;
+    for (int s = 0; s < size ; s++){
+      output += x.getData();
+      x = x.next();
     }
     return output;
   }
+  public Integer get(int index){
+    if (index == 0){
+      return start.getData();
+    }
+    if (index == size-1){
+      return end.getData();
+    }
+    Node output = start;
+    for (int x  = 1; x <= index; x++){
+      output = output.next();
+    }
+    return output.getData();
+  }
+  public Integer set(int index,Integer value){
+    int output = 
+    Node target = start;
+    for (int x  = 1; x <= index; x++){
+      output = target.next();
+    }
+
+  }
+
+  boolean contains(Integer value){}
+  int indexOf(Integer value){}
+
+  void add(int index,Integer value){}
+  Integer remove(int index){}
+  boolean remove(Integer value){}
 }
