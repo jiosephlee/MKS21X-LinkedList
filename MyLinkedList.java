9b55daf5d6dfaaf3084b23512daf1c6890690f9b
John Dogit config --global user.name John <jlee01@stuy.edu>
2018-12-13 14:32:49 -0500

    finished node

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,26 +1,62 @@
 public class MyLinkedList{
- private int size;
- private Node start,end;
 
- public MyLinkedList(int[] data){
+  private class Node{
 
- }
- public int size(){
-   return size;
- }
- public boolean add(int value){
-   Node oldEnd = end;
-   size++;
-   end = new Node(value, end, oldEnd);
-   return true;
- }
- public String toString(){
-   String output = "";
-   Node x = this.start;
-   for (int s = 0; s < size ; s++ ){
-     output += x.data();
-     x = x.next;
-   }
-   return output;
- }
+    private int data;
+    private Node next,prev;
+
+    public Node(int data, Node next, Node prev){
+      this.data = data;
+      this.next = next;
+      this.prev = prev;
+    }
+
+    public Node next(){
+      return next;
+    }
+    public Node prev(){
+      return prev;
+    }
+    public void setNext(Node other){
+      next = other;
+    }
+    public void setPrev(Node other){
+      prev = other;
+    }
+    public Integer getData(){
+      return data;
+    }
+    public Integer setData(Integer i){
+      data = i;
+    }
+    public String toString(){
+      String output = "" + data;
+      return output;
+    }
+  }
+
+  private int size;
+  private Node start,end;
+
+  public MyLinkedList(){
+    size = 0;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(int value){
+    Node oldEnd = end;
+    size++;
+    end = new Node(value, end, oldEnd);
+    return true;
+  }
+  public String toString(){
+    String output = "";
+    Node x = this.start;
+    for (int s = 0; s < size ; s++ ){
+      output += x.data();
+      x = x.next;
+    }
+    return output;
+  }
 }
